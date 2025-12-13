dfb7ef11377174a0731a0e1b5338a4be50c5bfcc
Sondre Eide Vik <sondre_vik@hotmail.com>
2025-12-13 20:15:57 +0100

    chore: cleanup

--- day1/target/debug/deps/day1-a5973b7fdcc3a078.d
+++
@@ -1,5 +0,0 @@
-C:\dev\AdventOfCode2025\day1\target\debug\deps\day1-a5973b7fdcc3a078.d: src\main.rs
-
-C:\dev\AdventOfCode2025\day1\target\debug\deps\libday1-a5973b7fdcc3a078.rmeta: src\main.rs
-
-src\main.rs:
--- day1/target/debug/deps/day1-f66eb722da0223d2.d
+++
@@ -1,5 +0,0 @@
-C:\dev\AdventOfCode2025\day1\target\debug\deps\day1-f66eb722da0223d2.d: src\main.rs
-
-C:\dev\AdventOfCode2025\day1\target\debug\deps\libday1-f66eb722da0223d2.rmeta: src\main.rs
-
-src\main.rs:
