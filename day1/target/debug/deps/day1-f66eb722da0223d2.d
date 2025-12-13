2e9379b9187a78928df66f32875cf88ae1964c79
Sondre Eide Vik <sondre_vik@hotmail.com>
2025-12-13 20:12:49 +0100

    feat: add example and puzzle data

---
+++ day1/target/debug/deps/day1-f66eb722da0223d2.d
@@ -0,0 +1,5 @@
+C:\dev\AdventOfCode2025\day1\target\debug\deps\day1-f66eb722da0223d2.d: src\main.rs
+
+C:\dev\AdventOfCode2025\day1\target\debug\deps\libday1-f66eb722da0223d2.rmeta: src\main.rs
+
+src\main.rs:
