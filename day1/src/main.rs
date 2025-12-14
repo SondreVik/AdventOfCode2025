ea5c51116e850d7058bf48f0fcc4bfd52e76db2c
Sondre Eide Vik <sondre_vik@hotmail.com>
2025-12-14 18:23:28 +0100

    feat: day1 part 2

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -3,6 +3,12 @@ use std::fs;
 fn main() {
     let filename = "src/data.txt";
     let file_contents = get_file(filename);
+    part1(&file_contents);
+    part2(&file_contents);
+}
+
+fn part1(file_contents: &str) {
+    println!("Part 1:");
     let file = file_contents.lines();
     let mut index = 50;
     let mut count = 0;
@@ -10,11 +16,32 @@ fn main() {
         let step: i32 = line[1..].parse().unwrap();
         let direction: char = line.chars().next().unwrap();
         let new_index = turn(index, &direction, step, 100 as i32);
+        if new_index == 0 {
+            count += 1
+        }
+        index = new_index;
+    }
+    println!("{}", count);
+}
+
+fn part2(file_contents: &str) {
+    println!("Part 2:");
+    let file = file_contents.lines();
+    let mut index = 50;
+    let mut count = 0;
+    let size = 100;
+    for line in file {
+        let step: i32 = line[1..].parse().unwrap();
+        let passes = step / size;
+        let actual_step = step % size;
+        let direction: char = line.chars().next().unwrap();
+        let new_index = turn(index, &direction, actual_step, size);
+        count += passes;
         if new_index == 0
             || direction == 'R' && new_index < index && index != 0
             || direction == 'L' && new_index > index && index != 0
         {
-            count += 1
+            count += 1;
         }
         index = new_index;
     }
