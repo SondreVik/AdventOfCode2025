592ec37c9e71bee9253ed6cfa288dff20e951452
Sondre Eide Vik <sondre_vik@hotmail.com>
2025-12-28 11:42:59 +0100

    feat: solve day 3 part 2

--- packages/aoc_2025_3/src/main.rs
+++ packages/aoc_2025_3/src/main.rs
@@ -2,10 +2,21 @@ fn main() {
     let _input = std::fs::read_to_string("input.txt").expect("Failed to read input file");
     let result = solve_1(&_input);
     println!("day3 part 1: {}", result);
+    let result = solve_2(&_input);
+    println!("day3 part 2: {}", result);
 }
 
 fn solve_1(input: &str) -> isize {
     let digits = 2;
+    let mut sum: isize = 0;
+    input.lines().for_each(|line| {
+        sum += find_max_number(line, digits).parse::<isize>().unwrap();
+    });
+    sum
+}
+
+fn solve_2(input: &str) -> isize {
+    let digits = 12;
     let mut sum: isize = 0;
     input.lines().for_each(|line| {
         sum += find_max_number(line, digits).parse::<isize>().unwrap();
@@ -49,6 +60,14 @@ mod tests {
         let example = load_example();
         let actual = solve_1(&example);
         let expected = 357;
+        assert_eq!(expected, actual)
+    }
+
+    #[test]
+    fn part2_works() {
+        let example = load_example();
+        let actual = solve_2(&example);
+        let expected = 3121910778619;
         assert_eq!(expected, actual)
     }
 }
