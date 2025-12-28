45fa3e727a5754eb7f564d2fb3b35200991edb63
Sondre Eide Vik <sondre_vik@hotmail.com>
2025-12-28 12:08:52 +0100

    feat: setup day 4

---
+++ packages/aoc_2025_4/src/main.rs
@@ -0,0 +1,19 @@
+fn main() {
+    let _input = std::fs::read_to_string("packages/aoc_2025_4/input.txt")
+        .expect("Failed to read input file");
+    todo!();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn load_example() -> String {
+        std::fs::read_to_string("example.txt").expect("Failed to read example file")
+    }
+
+    #[test]
+    fn it_works() {
+        todo!();
+    }
+}
